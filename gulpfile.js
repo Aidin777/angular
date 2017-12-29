bf8a5a6f20246e85ea6fc86a125c37c60e6f4b11
Aidin777 <arnold1990@yandex.ru>
2017-12-29 23:35:50 +0300

    Combined.js добавлено отслеживание файлов!

--- gulpfile.js
+++ gulpfile.js
@@ -5,22 +5,54 @@ var gulp              = require("gulp"),
     sass              = require("gulp-sass"),
     livereload        = require("gulp-livereload"),
     autoprefixer      = require("gulp-autoprefixer"),
-    concat            = require("gulp-concat");
+    concat            = require("gulp-concat"),
+    browserify        = require("browserify"),
+    vinylSource       = require("vinyl-source-stream");
+
 
 
 //Задачи
 gulp.task('css', function () {
 
 });
+//bundle - склейка файлов
+//Название comdined.js
+//место для хранения build
+gulp.task('js', function () {
 
+        return browserify('./assets/js/bootstrap.js')
+            .bundle().on('error', function(err){
+                console.log(err);
+                this.emit('end');
+            })
+            .pipe(vinylSource('combined.js'))
+            .pipe(gulp.dest('./build'));
 
+        //Angular
+        //d3
+        //Module
+        // stream -поток
+});
 
 
 //Отслеживание изменений
 gulp.task('dev', function () {
     livereload.listen();
 
+
+//пути относительно gulp.js
+    //Jade
     gulp.watch([
-    'assets/css/*.css'
-    ], function (event) {}).on('change', livereload.changed);
+        './views/*.jade',
+        './views/**/*.jade',
+        './views/**/**/*.jade'
+    ], function (e) {}).on('change', livereload.changed);
+
+    //Js
+    gulp.watch([
+        './assets/js/*.js',
+        './assets/js/**/*.js',
+        './assets/js/**/**/*.js'
+    ], ['js']).on('change', livereload.changed);
+
 });
--- server.js
+++ server.js
@@ -1,3 +1,28 @@
-/**
- * Created by Elvin on 26.12.2017.
- */
+var express           = require('express');
+var app               = express();
+var router            = express.Router();
+var path              = require('path');
+
+//установка чтения jade файлов
+app.set('view engine', 'jade');
+
+// скрыть заголовки
+app.disable('x-powered-by');
+
+app.use('/build', express.static(path.join(__dirname, 'build'),{
+    maxAge: 5000 * 1000
+}));
+app.use('/vendor', express.static(path.join(__dirname, 'vendor')));
+
+
+//прослойка middlewear
+app.get('/kit', function (req, res) {
+    res.render('main', {
+        pageTitle: 'Main Page'
+    });
+});
+
+
+app.use('', router);
+
+app.listen(8880);
